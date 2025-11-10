01d1af4aabcbffd4bb671a617d062fe5c4b60b09
gayathridevi <mudapakagayathridevi@gmail.com>
2025-11-10 20:48:57 +0530

    MediScribe AI

---
+++ MediScribe.AI-main/frontend/config/site.ts
@@ -0,0 +1,22 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Mediscribe.ai",
+  description: "Beautifully designed components built with Radix UI.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Transcribe",
+      href: "/Transcribe",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/vrtejus",
+    linkedin: "https://www.linkedin.com/in/vishnutejus/",
+    github: "https://github.com/vrtejus/MediScribe.AI",
+    docs: "https://github.com/vrtejus/MediScribe.AI",
+  },
+}
